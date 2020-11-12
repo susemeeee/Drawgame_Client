e7a4c4a7e80dccffd7e958f7b137c103d7a87ad6
susemeeee <hffg123@naver.com>
2020-11-12 10:51:00 +0900

    Add User data

---
+++ src/datatype/User.java
@@ -0,0 +1,18 @@
+/*
+ * User.java
+ * Author : 박찬형
+ * Created Date : 2020-11-11
+ */
+package datatype;
+
+import javax.swing.*;
+
+public class User {
+    private String name;
+    private ImageIcon characterIcon;
+
+    public User(String name, ImageIcon characterIcon){
+        this.name = name;
+        this.characterIcon = characterIcon;
+    }
+}
